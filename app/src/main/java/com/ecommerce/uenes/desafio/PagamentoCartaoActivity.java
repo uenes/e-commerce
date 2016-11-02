2c3e379f69f5871294df5ce3c20476a477d599de
uenes <ueneslucio@gmail.com>
2016-11-02 14:28:00 -0200

    setup do app

---
+++ app/src/main/java/com/ecommerce/uenes/desafio/PagamentoCartaoActivity.java
@@ -0,0 +1,13 @@
+package com.ecommerce.uenes.desafio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PagamentoCartaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento_cartao);
+    }
+}
